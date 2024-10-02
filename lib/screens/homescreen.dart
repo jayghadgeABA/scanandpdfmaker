b177e5224b1f3fdbb43c838897308aaa3194e306
jayghadgeABA <135590518+jayghadgeABA@users.noreply.github.com>
2024-10-02 15:45:00 +0300

    app launcher icon added

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:share_plus/share_plus.dart';
 
